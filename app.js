107cd6c07fb50147ec42ac69a367a02e9781430b
Bunma93 <bunma93@gmail.com>
2024-06-28 10:13:44 +0700

    Js-condition9

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // write code here
+let num1 = prompt("ค่าที่ 1");
+let num2 = prompt("ค่าที่ 2");
+let num3 = prompt("ค่าที่ 3");
+let values = [num1, num2, num3];
+let sortedValues = values.toSorted((a,b) => b - a);
+alert(sortedValues);
